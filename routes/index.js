84014e9c404b7ac9786b57910eb7b381661425b8
Mayur Saxena <mayur.saxena1997@gmail.com>
2022-11-29 22:52:56 -0500

    Initial Commit.

---
+++ routes/index.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var router = express.Router();
+var svgCaptcha = require('svg-captcha');
+
+const sharp = require('sharp');
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+/* GET home page. */
+router.get('/', async function(req, res, next) {
+  var captchaLength = 5
+  if (req.query.length) {
+    captchaLength = req.query.length
+  }
+  
+  //svgCaptcha.options.width=150 + ((captchaLength - 5) * 25)
+  //svgCaptcha.options.height=100
+  
+  var captcha = svgCaptcha.create({size: getRandomInt(1,8),
+                                   color: getRandomInt(0,1) ? false : true,
+                                   //background: '#cc9966',
+                                   noise: getRandomInt(0,5)});
+
+  let img = await sharp(Buffer.from(captcha.data), {density: 72}).png({compressionLevel: 1}).flatten({ background: { r: 255, g: 255, b: 255 } }).resize(150,50).toBuffer();
+
+  //console.log(await img.toBuffer())
+  //res.type('application/json')
+  if (req.query.view) {
+      res.render('train-captcha', {correct_code: captcha.text,
+                               pic: captcha.data,
+                               buffer: img.toString('base64')})
+  }
+  else {
+    res.json({img: Buffer.from(captcha.data, "utf-8").toString('base64'), answer: captcha.text});
+
+  }
+
+
+
+
+
+});
+
+module.exports = router;
